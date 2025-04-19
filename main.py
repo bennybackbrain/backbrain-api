aec438ae6f0543a0352eef9a1c3a305b24677ebf
bennybackbrain <bennibecker@aol.com>
2025-04-19 11:08:54 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Query
-from search_handler import search_files
-
-app = FastAPI()
-
-@app.get("/search-files")
-def search_files_route(query: str = Query(...)):
-    results = search_files(query)
-    return {"results": results}
